1bb98d5c690380542b434a740d5da9ddcbacb60e
SethRensei <sitaseth@yahoo.com>
2024-05-30 02:02:30 +0100

    Création de la nouvelle base de connaissance et refonte du code

--- app.py
+++ app.py
@@ -1,18 +1,21 @@
-from QaS import Conversation, CustomLLM, Embedding
 from langchain_core.output_parsers import StrOutputParser
 from langchain_core.runnables import RunnablePassthrough, RunnableParallel
+
+from Voice import Voice
+from QaS import Conversation, CustomLLM, Embedding
 
 emb = Embedding.getEmbedding('./LocalModel')
 retriever = Conversation.getDatabase(embeddings=emb, path='./vectors')
 
+llm = CustomLLM()
 prompt = Conversation.promptSystem()
 parser = StrOutputParser()
 
 result = RunnableParallel(context=retriever, question=RunnablePassthrough())
-llm = CustomLLM()
 chain = result | prompt | llm | parser
 
+voice = Voice()
 query = input('Votre question ? ')
 while query.lower() != 'sortir' :
-    print(f"{chain.invoke(query)}\n")
+    voice.say(f"{chain.invoke(query).replace('*', '')}\n")
     query = input('Votre question ? ')
